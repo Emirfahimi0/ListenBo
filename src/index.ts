5ba1e0fcd5ea71d59dc57320e5e79e3621f258ad
Emirfahimi0 <emirfahimi0@gmail.com>
2023-10-23 10:50:38 +0800

    install mongoose

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,15 @@ const PORT = 8989;
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
+
+/* 
+
+* The plan and features 
+* upload audio files
+* listen to single audio file
+* add to favorites
+* create new playlist
+* remove playlist (public/private)
+* remove audios
+*
+*/
