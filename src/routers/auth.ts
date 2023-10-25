5d7fa19e1994559e3b2586eac2616b42ece94fa8
Emirfahimi0 <emirfahimi0@gmail.com>
2023-10-25 13:28:59 +0800

    add middleware & refactor codes

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { userModel } from "../models";
+import { ICreatedUser } from "../types/user";
+import { createUserSchema } from "../utils/validation";
+import { validatorAuth } from "../middleware/validator";
 
 export const authRouter = Router();
 
-authRouter.post("/create", async (req, res) => {
-  const { name, email, password } = req.body();
-  const newUser = new userModel({ name, email, password });
-  newUser.save();
-  const createdUser = await userModel.create({ name, email, password });
-  res.json({ newUser: createdUser });
-});
+authRouter.post(
+  "/create",
+  validatorAuth(createUserSchema),
+  async (req: ICreatedUser, res) => {
+    const { name, email, password } = req.body;
+    createUserSchema.validate({ name, email, password });
+
+    // const newUser = new userModel({ name, email, password });
+    // newUser.save();
+    const createdUser = await userModel.create({ name, email, password });
+    return res.json({ newUser: createdUser });
+  }
+);
