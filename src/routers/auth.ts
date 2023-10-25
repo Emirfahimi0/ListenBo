d7463c88cae689a1a8cd315dc4ecff117dac304a
Emirfahimi0 <emirfahimi0@gmail.com>
2023-10-25 15:40:32 +0800

    refactor & enhance code base

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,21 +1,8 @@
 import { Router } from "express";
-import { userModel } from "../models";
-import { ICreatedUser } from "../types/user";
 import { createUserSchema } from "../utils/validation";
 import { validatorAuth } from "../middleware/validator";
+import { createUser } from "../controller";
 
 export const authRouter = Router();
 
-authRouter.post(
-  "/create",
-  validatorAuth(createUserSchema),
-  async (req: ICreatedUser, res) => {
-    const { name, email, password } = req.body;
-    createUserSchema.validate({ name, email, password });
-
-    // const newUser = new userModel({ name, email, password });
-    // newUser.save();
-    const createdUser = await userModel.create({ name, email, password });
-    return res.json({ newUser: createdUser });
-  }
-);
+authRouter.post("/create", validatorAuth(createUserSchema), createUser);
