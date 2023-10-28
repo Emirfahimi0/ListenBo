4f54a13e4fdf862824a942f1d4f75cca3b825772
Emirfahimi0 <emirfahimi0@gmail.com>
2023-10-28 10:57:24 +0800

    add new types

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,22 +1,43 @@
 import { Request } from "express";
 import { ObjectId } from "mongoose";
 
-declare interface IUserDocument {
-  avatarFile?: { url: string; publicId: string };
-  email: string;
-  favourites: ObjectId[];
-  followers: ObjectId[];
-  followings: ObjectId[];
-  name: string;
-  password: string;
-  tokens: string[];
-  verified: boolean;
-}
-
-declare interface ICreatedUser extends Request {
-  body: {
+declare global {
+  interface IUserDocument {
+    avatarFile?: { url: string; publicId: string };
     email: string;
+    favourites: ObjectId[];
+    followers: ObjectId[];
+    followings: ObjectId[];
     name: string;
     password: string;
-  };
+    tokens: string[];
+    verified: boolean;
+  }
+
+  interface ICreatedUser extends Request {
+    body: {
+      email: string;
+      name: string;
+      password: string;
+    };
+  }
+  interface ITokenMethods {
+    compareToken: (token: string) => Promise<boolean>;
+  }
+
+  interface IEmailOptions {
+    banner: string;
+    buttonTitle: string;
+    label?: string;
+    link: string;
+    logo: string;
+    message: string;
+    requestMessage: string;
+    title: string;
+  }
+  interface IEmailVerifiedToken {
+    createdAt: Date;
+    owner: ObjectId;
+    token: string;
+  }
 }
