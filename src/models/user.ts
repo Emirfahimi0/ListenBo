bb367e96c20171e9697fbfac05c52078a2a09178
Emirfahimi0 <emirfahimi0@gmail.com>
2023-10-28 11:00:17 +0800

    update models for emails

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
+import { compare, hash } from "bcrypt";
 import { Model, Schema, model } from "mongoose";
-import { IUserDocument } from "../types/user";
 
 const userSchema = new Schema<IUserDocument>(
   {
@@ -49,6 +49,45 @@ const userSchema = new Schema<IUserDocument>(
     ],
   },
   { timestamps: true }
+);
+
+const emailVerifiedTokenSchema = new Schema<
+  IEmailVerifiedToken,
+  {},
+  ITokenMethods
+>({
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    expires: 3600, // 60 min * 60 sec = 3600s;
+    default: Date.now(),
+  },
+});
+
+emailVerifiedTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    const enCryptedResult = (this.token = await hash(this.token, 10));
+  }
+  next();
+});
+
+emailVerifiedTokenSchema.methods.compareToken = async function (token) {
+  const compareToken = await compare(token, this.token);
+  return compareToken;
+};
+
+export const emailTokenModel: Model<IEmailVerifiedToken> = model(
+  "EmailVerifiedToken",
+  emailVerifiedTokenSchema
 );
 
 export const userModel: Model<IUserDocument> = model("User", userSchema);
