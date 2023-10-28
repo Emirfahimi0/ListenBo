f046ea3e8969ef101f7d9fb1d0ea9a326c04144a
Emirfahimi0 <emirfahimi0@gmail.com>
2023-10-28 10:58:11 +0800

    update utils

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,3 +1,14 @@
 const { env: ENV } = process as { env: { [key: string]: string } };
 
-export const MONGO_URI = ENV.MONGO_URI as string;
+// export const MONGO_URI = ENV.MONGO_URI as string;
+// export const MAIL_TRAP_USER = ENV.MAIL_TRAP_USER as string;
+// export const MAIL_TRAP_PASSWORD = ENV.MAIL_TRAP_USER as string;
+
+export const {
+  MONGO_URI,
+  MAIL_TRAP_USER,
+  MAIL_TRAP_PASS,
+  MAIL_TRAP_SENDER,
+  GMAIL_APP_USE,
+  GMAIL_USER,
+} = ENV;
