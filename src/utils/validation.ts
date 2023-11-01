90f57a4c9ec9d1f0388acfe0341bc5b9b2014d5d
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-01 21:15:11 +0800

    update utils

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,31 +1,30 @@
 import { isValidObjectId } from "mongoose";
 import * as yup from "yup";
+import { ENGLISH, PASSREGEX } from "../constant";
 
+const { VALIDATION } = ENGLISH;
 export const createUserValidation = yup.object().shape({
   name: yup
     .string()
     .trim()
-    .required("Name is missing!")
-    .min(3, "Name is too short!")
-    .max(20, "Name is too long!"),
+    .required(VALIDATION.NAME_MISSING)
+    .min(3, VALIDATION.NAME_SHORT)
+    .max(20, VALIDATION.NAME_LONG),
 
   email: yup
     .string()
     .trim()
-    .required("Email is missing")
-    .email("Invalid email id!"),
+    .required(VALIDATION.EMAIL_ADDRESS_MISSING)
+    .email(VALIDATION.EMAIL_INVALID),
   password: yup
     .string()
-    .min(8, "Password is too short!")
-    .required("Password is missing!")
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-    ),
+    .required(VALIDATION.PASSWORD_MISSING)
+    .min(8, VALIDATION.PASSWORD_SHORT)
+    .matches(PASSREGEX, VALIDATION.PASSWORD_MATCHES),
 });
 
 export const emailTokenVerification = yup.object().shape({
-  token: yup.string().trim().required("Invalid token!"),
+  token: yup.string().trim().required(VALIDATION.INVALID_TOKEN),
   userId: yup
     .string()
     .transform(function (value) {
@@ -34,11 +33,11 @@ export const emailTokenVerification = yup.object().shape({
       }
       return "";
     })
-    .required("Invalid user Id"),
+    .required(VALIDATION.INVALID_USER_ID),
 });
 
 export const updatePasswordValidation = yup.object().shape({
-  token: yup.string().trim().required("Invalid token!"),
+  token: yup.string().trim().required(VALIDATION.INVALID_TOKEN),
   userId: yup
     .string()
     .transform(function (value) {
@@ -47,15 +46,20 @@ export const updatePasswordValidation = yup.object().shape({
       }
       return "";
     })
-    .required("Invalid user Id"),
+    .required(VALIDATION.INVALID_USER_ID),
   password: yup
     .string()
-    .min(8, "Password is too short!")
-    .required("Password is missing!")
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-    ),
+    .required(VALIDATION.PASSWORD_MISSING)
+    .min(8, VALIDATION.PASSWORD_SHORT)
+    .matches(PASSREGEX, VALIDATION.PASSWORD_MATCHES),
+});
+
+export const signInValidation = yup.object().shape({
+  email: yup
+    .string()
+    .required(VALIDATION.EMAIL_ADDRESS_MISSING)
+    .email(VALIDATION.EMAIL_INVALID),
+  password: yup.string().trim().required(VALIDATION.PASSWORD_MISSING),
 });
 
 export const isArrayNotEmpty = (value: unknown[] | null | undefined) => {
