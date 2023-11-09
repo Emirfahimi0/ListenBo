8b3ed9ae9849f1f1bdf8f60aa907260895a654dc
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-09 10:32:20 +0800

    update types

--- src/types/audioT.d.ts
+++ src/types/audioT.d.ts
@@ -1,12 +1,15 @@
 type TCategoriesPlaylist =
-  | "Art"
-  | "Business"
-  | "Education"
-  | "Entertainment"
-  | "Kids & Family"
-  | "Music"
-  | "Science"
-  | "Tech"
-  | "Others";
+  | 'Art'
+  | 'Business'
+  | 'Education'
+  | 'Entertainment'
+  | 'Kids & Family'
+  | 'Music'
+  | 'Science'
+  | 'Tech'
+  | 'Others';
 
-type TPopulateFavList = IAudioDocument<{ _id: ObjectId; name: string }>[];
+type TPopulateList = IAudioDocument<{ _id: ObjectId; name: string }>[];
+
+type TPlaylistVisibility = 'Public' | 'Private' | 'auto';
+type TPlaylistDefaultVisibility = 'Public' | 'Private';
