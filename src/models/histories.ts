63e36d4be4663d810e94affeb88b57c31150bfcb
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-09 17:28:08 +0800

    added new model (histories)

---
+++ src/models/histories.ts
@@ -0,0 +1,27 @@
+import { Model, Schema, model, models } from 'mongoose';
+
+const historiesSchema = new Schema<IHistoriesDocument>(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    all: [
+      {
+        audio: { type: Schema.Types.ObjectId, ref: 'Audio' },
+        progess: Number,
+        date: { type: Date, required: true },
+      },
+    ],
+    last: {
+      audio: { type: Schema.Types.ObjectId, ref: 'Audio' },
+      progess: Number,
+      date: { type: Date, required: true },
+    },
+  },
+  { timestamps: true },
+);
+
+export const historysModel: Model<IHistoriesDocument> =
+  models.History || model('History', historiesSchema);
