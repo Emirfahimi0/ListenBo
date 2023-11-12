009a733d3449a09abac8c40f290b95c001b88913
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-12 20:58:42 +0800

    update routes

--- src/routers/histories.ts
+++ src/routers/histories.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyAuth } from "../middleware/auth";
-import { getAllHistories, removeHistory, updateHistory } from "../controller";
+import { getAllHistories, getRecentlyPlayed, removeHistory, updateHistory } from "../controller";
 import { updateHistoryValidation } from "../utils";
 import { validator } from "../middleware/validator";
 
@@ -9,3 +9,4 @@ export const historiesRouter = Router();
 historiesRouter.post("/", verifyAuth, validator(updateHistoryValidation), updateHistory);
 historiesRouter.delete("/", verifyAuth, removeHistory);
 historiesRouter.get("/", verifyAuth, getAllHistories);
+historiesRouter.get("/recently-played", verifyAuth, getRecentlyPlayed);
