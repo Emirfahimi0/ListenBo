e01ae59bf92c74e5b56ecf8a8d012bd992001c1b
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-13 14:29:53 +0800

    update types

--- src/types/audioT.d.ts
+++ src/types/audioT.d.ts
@@ -1,23 +1,24 @@
 type TCategoriesPlaylist =
-  | 'Art'
-  | 'Business'
-  | 'Education'
-  | 'Entertainment'
-  | 'Kids & Family'
-  | 'Music'
-  | 'Science'
-  | 'Tech'
-  | 'Others';
+	| "Art"
+	| "Business"
+	| "Education"
+	| "Entertainment"
+	| "Kids & Family"
+	| "Music"
+	| "Science"
+	| "Tech"
+	| "Others";
 
 type TPopulateList = IAudioDocument<{ _id: ObjectId; name: string }>[];
+type TPopulate = IAudioDocument<{ _id: ObjectId; name: string }>;
 
-type TPlaylistVisibility = 'Public' | 'Private' | 'auto';
-type TPlaylistDefaultVisibility = 'Public' | 'Private';
+type TPlaylistVisibility = "Public" | "Private" | "auto";
+type TPlaylistDefaultVisibility = "Public" | "Private";
 
 type TPaginationQuery = { pageNo: string; limit: string };
 
 interface IHistoryContent {
-  audio: ObjectId;
-  date: Date;
-  progress: number;
+	audio: ObjectId;
+	date: Date;
+	progress: number;
 }
