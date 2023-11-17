cde45f0f6fa2c64347333e1a0d788dfd0597ccb4
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-17 23:20:30 +0800

    added error handler function

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-import express from 'express';
-import 'dotenv/config';
-import './db';
-import {
-  audioRouter,
-  authRouter,
-  favouriteRouter,
-  historiesRouter,
-  playlistRouter,
-  profileRouter,
-} from './routers';
+import express from "express";
+import "dotenv/config";
+import "express-async-errors";
+import "./db";
+import { audioRouter, authRouter, favouriteRouter, historiesRouter, playlistRouter, profileRouter } from "./routers";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('src/public'));
+app.use(express.static("src/public"));
 
-app.use('/auth', authRouter);
-app.use('/audio', audioRouter);
-app.use('/favourite', favouriteRouter);
-app.use('/playlist', playlistRouter);
-app.use('/profile', profileRouter);
-app.use('/history', historiesRouter);
+app.use("/auth", authRouter);
+app.use("/audio", audioRouter);
+app.use("/favourite", favouriteRouter);
+app.use("/playlist", playlistRouter);
+app.use("/profile", profileRouter);
+app.use("/history", historiesRouter);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8989;
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+	console.log(`Listening on ${PORT}`);
 });
 
 /* 
