c32e4527b035054d9d8b65e336ac7eb3cbc04676
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-19 16:35:42 +0800

    wip : added nescessary files

---
+++ client/ListenBo/src/styles/color.ts
@@ -0,0 +1,34 @@
+export const colorTransparent = "rgba(0,0,0,0)";
+
+export const colorBlack = {
+  _1: "#000000",
+  _1_4: "rgba(0,0,0,0.4)",
+  _1_7: "rgba(0,0,0,0.7)",
+  _2: "#171717",
+  _3: "0F0F0F",
+};
+
+export const colorRose = {
+  _1: "BCABAE",
+  _2: "5B494C",
+  _3: "C9BBBD",
+};
+
+export const colorJet = {
+  _1: "2D2E2E",
+  _2: "2D2E2E",
+  _3: "2D2E2E",
+  _4: "282929",
+};
+
+export const colorGray = {
+  _1: "716969",
+  _2: "554E4E",
+  _3: "C4BFBF",
+};
+
+export const colorWhite = {
+  _1: "FBFBFB",
+  _2: "FBFBFB",
+  _3: "FBFBFB",
+};
