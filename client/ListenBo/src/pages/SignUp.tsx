4e69b6fd262e68eb0dee8c28d1eaa67dbf8e6513
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-27 22:44:44 +0800

    update Sign Up page

--- client/ListenBo/src/pages/SignUp.tsx
+++ client/ListenBo/src/pages/SignUp.tsx
@@ -1,5 +1,17 @@
-import React, { Fragment, FunctionComponent } from "react";
-import { SafeAreaView, TouchableOpacity, View, ViewStyle, Image, Text, ScrollView } from "react-native";
+import React, { Fragment, FunctionComponent, useState } from "react";
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+  Image,
+  Text,
+  NativeSyntheticEvent,
+  TextInputFocusEventData,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+} from "react-native";
 import {
   alignItemsEnd,
   alignSelfCenter,
@@ -11,14 +23,12 @@ import {
   flexRow,
   fs12BoldGray1,
   fs12RegRose2,
-  fs16BoldWhite1,
   fsAlignCenter,
   justifyContentStart,
   sh10,
   sh152,
   sh16,
   sh4,
-  sh8,
   sw12,
   sw152,
   sw16,
@@ -30,15 +40,115 @@ import {
 import { LANGUAGE } from "../constants";
 import { CustomButton, CustomSpacer, CustomTextInput } from "../components";
 import { Icon } from "../components/Icons/";
+import { isPasswordValid, isValidEmail } from "../utils";
 const { WELCOME_PAGE, FORM_LABEL } = LANGUAGE;
 
 export const SignUp: FunctionComponent = () => {
-  const handleSignIn = () => {
-    console.log("first");
+  const [signUp, setSignUp] = useState<ISignUpForm>({
+    email: "",
+    name: "",
+    password: "",
+    errorEmail: undefined,
+    errorName: undefined,
+    errorPassword: undefined,
+  });
+  const { email, name, password, errorEmail, errorName, errorPassword } = signUp;
+  const [loading, setLoading] = useState<boolean | undefined>(undefined);
+  const [visibility, setVisiblity] = useState<boolean | undefined>(undefined);
+
+  const handleOnChangeEmail = (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    setSignUp({ ...signUp, ...{ email: event.nativeEvent.text } });
   };
 
+  const handleOnChangeName = (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    setSignUp({ ...signUp, ...{ name: event.nativeEvent.text } });
+  };
+
+  const handleOnChangePassword = (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    setVisiblity(true);
+    setSignUp({ ...signUp, ...{ password: event.nativeEvent.text } });
+  };
+
+  const handleOnBlurEmail = () => {
+    let error = "";
+    const { email } = signUp;
+    if (email === "") {
+      error = FORM_LABEL.EMAIL_ADDRESS_EMPTY;
+      return setSignUp({ ...signUp, ...{ errorEmail: error } });
+    }
+
+    if (isValidEmail(email) === false) {
+      error = FORM_LABEL.ERROR_INVALID_EMAIL;
+      return setSignUp({ ...signUp, ...{ errorEmail: error } });
+    }
+
+    return setSignUp({ ...signUp, ...{ errorEmail: undefined } });
+  };
+
+  const handleOnBlurName = () => {
+    let error = "";
+    const { name } = signUp;
+
+    if (name.trim() === "") {
+      error = FORM_LABEL.NAME_EMPTY;
+      return setSignUp({ ...signUp, ...{ errorName: error } });
+    }
+    if (name.length < 3) {
+      error = FORM_LABEL.NAME_MIN;
+      return setSignUp({ ...signUp, ...{ errorName: error } });
+    }
+    if (name.length > 20) {
+      error = FORM_LABEL.NAME_MAX;
+      return setSignUp({ ...signUp, ...{ errorName: error } });
+    }
+
+    return setSignUp({ ...signUp, ...{ errorName: undefined } });
+  };
+
+  const handleOnBlurPassword = () => {
+    let error = "";
+    const { password } = signUp;
+
+    if (password === "") {
+      error = FORM_LABEL.PASSWORD_EMPTY;
+      return setSignUp({ ...signUp, ...{ errorPassword: error } });
+    }
+    if (isPasswordValid(password) === false) {
+      error = FORM_LABEL.PASSWORD_INVALID;
+      return setSignUp({ ...signUp, ...{ errorPassword: error } });
+    }
+
+    return setSignUp({ ...signUp, ...{ errorPassword: undefined } });
+  };
+
+  const handleOnFocusEmail = () => {
+    setSignUp({ ...signUp, ...{ errorEmail: undefined } });
+  };
+  const handleOnFocusName = () => {
+    setSignUp({ ...signUp, ...{ errorName: undefined } });
+  };
+  const handleOnFocusPassword = () => {
+    setSignUp({ ...signUp, ...{ errorPassword: undefined } });
+  };
+
+  const handleSignUp = () => {
+    let request: IRequestBody = {
+      name: signUp.name,
+      email: signUp.email,
+      password: signUp.password,
+    };
+  };
+
+  const setSecureText = () => {
+    return setVisiblity(!visibility);
+  };
+  const disable =
+    (signUp.email !== "" && signUp.errorEmail !== undefined) ||
+    (signUp.password !== "" && signUp.errorPassword !== undefined) ||
+    (signUp.name !== "" && signUp.errorName !== undefined);
+
   return (
-    <View style={backgroundStyle}>
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={backgroundStyle}>
       <SafeAreaView style={{ paddingVertical: sh10 }}>
         <View style={{ ...flexRow, ...justifyContentStart }}>
           <TouchableOpacity style={arrowStyle}>
@@ -53,44 +163,68 @@ export const SignUp: FunctionComponent = () => {
         <CustomTextInput
           label={FORM_LABEL.EMAIL_ADDRESS_LABEL}
           labelHolder={WELCOME_PAGE.ENTER_EMAIL_LABEL}
-          error={"Not a valid email address!"}
+          error={errorEmail}
+          value={email}
+          onChange={handleOnChangeEmail}
+          onBlur={handleOnBlurEmail}
+          onFocus={handleOnFocusEmail}
+          keyboardType="email-address"
           containerStyle={{ paddingVertical: sh4 }}
         />
+
         <CustomTextInput
           label={FORM_LABEL.NAME_LABEL}
+          onChange={handleOnChangeName}
+          onBlur={handleOnBlurName}
+          error={errorName}
+          onFocus={handleOnFocusName}
+          value={name}
           labelHolder={FORM_LABEL.NAME_PLACEHOLDER}
           containerStyle={{ paddingVertical: sh4 }}
-          error={"..."}
         />
         <CustomTextInput
+          autoCapitalize="none"
+          containerStyle={{ paddingVertical: sh4 }}
+          error={errorPassword}
           label={FORM_LABEL.PASSWORDS_LABEL}
           labelHolder={FORM_LABEL.PASSWORD_PLACEHOLDER}
-          containerStyle={{ paddingVertical: sh4 }}
+          onBlur={handleOnBlurPassword}
+          onChange={handleOnChangePassword}
+          onFocus={handleOnFocusPassword}
+          secureTextEntry={visibility}
+          setVisible={setSecureText}
+          value={password}
+          visibilityText={visibility}
         />
-        <View style={{ ...alignItemsEnd, paddingRight: sw8, paddingVertical: sh10 }}>
-          <Text style={fs12BoldGray1}>Forgot Password?</Text>
-        </View>
+        <Pressable disabled={loading}>
+          <View style={{ ...alignItemsEnd, paddingRight: sw8, paddingVertical: sh10 }}>
+            <Text style={fs12BoldGray1}>Forgot Password?</Text>
+          </View>
+        </Pressable>
+
         <Fragment>
           <View style={alignSelfCenter}>
             <CustomButton
-              onPress={handleSignIn}
+              onPress={handleSignUp}
+              withDebounce={true}
               text={WELCOME_PAGE.SIGN_UP_LABEL}
               buttonStyle={{ borderRadius: sw16 }}
               textStyle={fsAlignCenter}
-              disabled={false}
+              disabled={disable}
+              loading={loading}
             />
           </View>
           <CustomSpacer space={sh4} />
           <View style={{ ...flexRow, ...centerHorizontal }}>
             <Text style={fs12BoldGray1}>{WELCOME_PAGE.ALREADY_HAVE_ACCOUNT_LABEL}</Text>
             <CustomSpacer isHorizontal={true} space={sw4} />
-            <TouchableOpacity>
+            <TouchableOpacity disabled={loading}>
               <Text style={fs12RegRose2}>{WELCOME_PAGE.SIGN_IN_LABEL}</Text>
             </TouchableOpacity>
           </View>
         </Fragment>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
