f13ce061bb5cca88ae9593fd0c1f2e0a9178e20f
Emirfahimi0 <emirfahimi0@gmail.com>
2023-12-05 20:51:10 +0800

    update types

--- client/ListenBo/src/types/navigation.d.ts
+++ client/ListenBo/src/types/navigation.d.ts
@@ -2,11 +2,12 @@ import { ParamListBase } from "@react-navigation/native";
 
 declare global {
   interface INavigationParamsList extends ParamListBase {
-    SignUp: undefined;
     OnBoarding: undefined;
+    SignIn: undefined;
+    SignUp: undefined;
   }
 
   type IStackNavigationProp = import("@react-navigation/stack").StackNavigationProp<INavigationParamsList>;
 
-  type TPageType = "OnBoarding" | "SignUp";
+  type TPageType = "OnBoarding" | "SignUp" | "SignIn";
 }
