53837f19ec6476cbd0db03c99e31d578e9bddf19
Emirfahimi0 <emirfahimi0@gmail.com>
2023-12-06 13:57:20 +0800

    remove unused navigation function

--- client/ListenBo/src/navigation/Navigation.ts
+++
@@ -1,5 +0,0 @@
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-
-export const navigate = (screens: TPageType) => {
-  return useNavigation<NavigationProp<INavigationParamsList>>().navigate(screens);
-};
