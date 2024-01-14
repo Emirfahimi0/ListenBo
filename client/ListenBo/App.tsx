563f0ade53095423016f7b475695ebb17d1ba2b6
Emirfahimi0 <emirfahimi0@gmail.com>
2024-01-14 14:17:09 +0800

    update apps

--- client/ListenBo/App.tsx
+++ client/ListenBo/App.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { PublicRoute } from "./src/navigation";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { flexChild } from "./src/styles";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
+import { MainRoutes } from "./src/navigation";
 
 const App = () => {
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={flexChild}>
         <BottomSheetModalProvider>
-          <NavigationContainer>
-            <PublicRoute />
-          </NavigationContainer>
+          <MainRoutes />
         </BottomSheetModalProvider>
       </GestureHandlerRootView>
     </Provider>
